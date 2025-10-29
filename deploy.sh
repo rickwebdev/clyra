a7e377401437b0dbbd4b4f15413e30e09d3a8c3b
rickwebdev <rickwebdev@users.noreply.github.com>
2025-10-29 19:03:09 -0400

    Deploy update

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ git push origin main
 
 # Deploy to server
 echo "🔄 Deploying to server..."
-ssh root@159.65.45.45 "cd /home/n30m0rph/web/clyrastudios.com/nodejs && git pull origin main && source /root/.nvm/nvm.sh && nvm use 18 && npm run build && pkill -f 'next start' && nohup npm start > app.log 2>&1 &"
+ssh root@159.65.45.45 "cd /home/n30m0rph/web/clyrastudios.com/nodejs && git pull origin main && source /root/.nvm/nvm.sh && nvm use 18 && npm run build && cp -r out/* /home/n30m0rph/web/clyrastudios.com/public_html/ && pkill -f 'next start' && nohup npm start > app.log 2>&1 &"
 
 echo "✅ Deployment complete!"

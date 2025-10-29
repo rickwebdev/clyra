f51359226d2331ca8e0f0b6f1d4013ace5a7354e
rickwebdev <rickwebdev@users.noreply.github.com>
2025-10-29 01:31:57 -0400

    Deploy update

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,5 @@ git push origin main
 
 # Deploy to server
 echo "🔄 Deploying to server..."
-ssh root@159.65.45.45 "cd /home/n30m0rph/web/clyrastudios.com/nodejs && git pull origin main && npm run build && pm2 restart wp-studio"
-
+ssh root@159.65.45.45 "cd /home/n30m0rph/web/clyrastudios.com/nodejs && git pull origin main && /root/.nvm/versions/node/v18.20.8/bin/npm run build && pm2 restart wp-studio"
 echo "✅ Deployment complete!"
